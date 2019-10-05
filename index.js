f37e054f3d002d5b304321d4206a83f82d7081ce
yeyanxiang <yeyanxiang@cnbn.cn>
2019-10-05 17:29:39 +0800

    fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 hexo.extend.filter.register('before_post_render', function (data) {
+
     console.info("\n--------------------------- " + data.title + " ---------------------------\nsource: " + data.source);
-    var regExp = RegExp("!\\[(.*)\\]\\((.*)\\)", "g");
-    data.content = data.content.replace(regExp, function (substring, name, url) {
+
+    data.content = data.content.replace("[TOC]", function (substring) {
+        console.info("replace " + substring + " -> " + "<!-- toc -->");
+        return "<!-- toc -->";
+    });
+
+    data.content = data.content.replace(RegExp("!\\[(.*)\\]\\((.*)\\)", "g"), function (substring, name, url) {
         if (url.indexOf("://") === -1) {
             var index = url.indexOf("/");
             if (index !== -1) {
@@ -13,10 +19,18 @@ hexo.extend.filter.register('before_post_render', function (data) {
         return result;
     });
 
-    data.content = data.content.replace("[TOC]", function (substring) {
-        console.info("replace " + substring + " -> " + "<!-- toc -->");
-        return "<!-- toc -->";
+    data.content = data.content.replace(RegExp("\\[(.*)\\]\\((.*)\\)", "g"), function (substring, name, url) {
+        if (url.indexOf("://") === -1) {
+            var index = url.indexOf("/");
+            if (index !== -1) {
+                url = url.substr(index + 1);
+            }
+        }
+        var result = "{% asset_img " + url + " " + name + " %}";
+        console.info("replace " + substring + " -> " + result);
+        return result;
     });
+
     console.info("\n");
     return data;
 }, 9);
