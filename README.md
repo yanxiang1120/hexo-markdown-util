bbd9f9feb37263d9e6c11c11c79c9c1196b732fc
yeyanxiang <yeyanxiang@cnbn.cn>
2019-10-05 15:37:38 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hexo-markdown-util
+
 hexo markdown 插件，兼容markdown图片和toc语法。
+
+## 开始使用
+
+```bash
+npm install https://github.com/yanxiang1120/hexo-markdown-util.git --save
+# clean当前项目
+hexo clean 
+# 重新生成静态文件
+hexo g
+```
+
+## 转换结果
+
+`![img1](img/img1.png)` -> `{% asset_img img1.png img1 %}`
+
+`![img2](img2.png)` -> `{% asset_img img2.png img2 %}`
+
+`![img3](http://www.example/img3.png)` -> `{% asset_img http://www.example/img3.png img3 %}`
+
+`[TOC]` -> `<!-- toc -->`
---
+++ index.js
@@ -0,0 +1,17 @@
+hexo.extend.filter.register('before_post_render', function (data) {
+    var regExp = RegExp("!\\[(.*)\\]\\((.*)\\)", "g");
+    data.content = data.content.replace(regExp, function (substring, $1, $2, $3) {
+        if (url.indexOf("://") === -1) {
+            var index = url.indexOf("/");
+            if (index !== -1) {
+                url = url.substr(index + 1);
+            }
+        }
+        var result = "{% asset_img " + url + " " + name + " %}";
+        console.info("replace " + substring + " -> " + result)
+        return result;
+    });
+
+    data.content = data.content.replace("[TOC]", "<!-- toc -->");
+    return data;
+}, 9);
