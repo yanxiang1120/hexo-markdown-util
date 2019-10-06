38cb4f5c23adcf7c8091cf9a9eb76d0a3ef93e3b
yeyanxiang <yeyanxiang@cnbn.cn>
2019-10-06 16:20:37 +0800

    取消toc替换

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ hexo.extend.filter.register('before_post_render', function (data) {
 
     console.info("\n--------------------------- " + data.title + " ---------------------------\nsource: " + data.source);
 
-    data.content = data.content.replace("[TOC]", function (substring) {
-        console.info("replace " + substring + " -> " + "<!-- toc -->");
-        return "<!-- toc -->";
-    });
+    // data.content = data.content.replace("[TOC]", function (substring) {
+    //     console.info("replace " + substring + " -> " + "<!-- toc -->");
+    //     return "<!-- toc -->";
+    // });
 
     data.content = data.content.replace(RegExp("!\\[(.*)\\]\\((.*)\\)", "g"), function (substring, name, url) {
         if (url.indexOf("://") === -1) {
